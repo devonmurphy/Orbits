b826ca8336e28e79e3431881d4467a4c71b609a7
Devon Murphy <devonmurphy1@gmail.com>
2019-07-20 22:09:34 -0700

    added mocha into the mix

--- tests/login.js
+++ tests/login.js
@@ -4,38 +4,62 @@ var assert = require('assert');
 var expect = require('chai').expect;
 var utils = require('../server/utils.js');
 
-(async () => {
-  var login = async function (url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: ['load', 'domcontentloaded'] });
-    console.log('joining from: ' + url);
-    await page.waitForSelector('#inGameName');
-    await page.type('#inGameName', 'hello world', { delay: 20 })
 
-    await page.waitForSelector('#submitInGameName');
-    await page.click('#submitInGameName');
-    await page.waitForSelector('#quickMatch');
-    await page.click('#quickMatch');
+// puppeteer options
+const opts = {
+  //headless: false,
+  //slowMo: 100,
+  timeout: 10000
+};
 
-    await page.waitForSelector('canvas');
-    await page.keyboard.down('w');
+var login = async function (url) {
+  const browser = await puppeteer.launch(opts);
+  const page = await browser.newPage();
+  await page.goto('http://localhost:5000/game', { waitUntil: ['load', 'domcontentloaded'] });
+  await page.waitForSelector('#inGameName');
+  await page.type('#inGameName', 'hello world', { delay: 20 })
 
-    //await browser.close();
-    return { browser, page };
-  }
-  var url0 = 'http://localhost:5000/game';
-  var url1 = 'http://' + utils.getIPAddress() + ':5000/game';
-  var instance0 = await login(url0);
-  var instance1 = await login(url1);
+  await page.waitForSelector('#submitInGameName');
+  await page.click('#submitInGameName');
+  await page.waitForSelector('#quickMatch');
+  await page.click('#quickMatch');
 
-  await instance0.page.waitForSelector('#playAgainBtn');
-  await instance0.page.click('#playAgainBtn');
-  await instance1.page.waitForSelector('#playAgainBtn');
-  await instance1.page.click('#playAgainBtn');
+  await page.waitForSelector('canvas');
+  await page.keyboard.down('w');
 
-  //await instance0.page.screenshot({ path: 'example.png' });
+  //await browser.close();
+  return { browser, page };
+}
 
-  await instance0.browser.close();
-  await instance1.browser.close();
-})();
+describe('login test', function () {
+  var instance0;
+  var instance1;
+
+  before(async function () {
+    this.timeout(20000);
+    instance0 = await login();
+    instance1 = await login();
+  });
+
+  after(async function () {
+
+  })
+
+  it('should be able to login', async function () {
+    var result = false;
+    this.timeout(20000);
+
+    try {
+      await instance0.page.waitForSelector('#playAgainBtn');
+      await instance1.page.waitForSelector('#playAgainBtn');
+      result = true;
+    } catch (error) {
+      console.log(error);
+    }
+
+    expect(result).to.be.true;
+    await instance0.browser.close();
+    await instance1.browser.close();
+  });
+
+});
