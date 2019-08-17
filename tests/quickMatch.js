646805470aff1ed51944325c874f199c28120b3e
Devon Murphy <devonmurphy1@gmail.com>
2019-08-17 14:19:13 -0700

    add quickmatch test

--- tests/quickMatch.js
+++ tests/quickMatch.js
@@ -37,62 +37,17 @@ var instance1;
 describe('running quick match tests', function () {
 
 
-  it('players can login', async function () {
-    instance0 = await login();
-    instance1 = await login();
-  }).timeout(20000);
 
-  it('players can join quick match', async function () {
-    await joinQuickMatch(instance0);
-    await joinQuickMatch(instance1);
-  });
+  it('test playing with 200 players in 100 quick matches', async function () {
+    var joinPlayer = async function () {
+      var newPlayer = await login();
+      await joinQuickMatch(newPlayer);
+      await newPlayer.browser.close();
+    }
 
-  it('players can die to distance', async function () {
-    // register that the players have hit thrust forward
-    await instance0.page.keyboard.down('w');
-    await instance1.page.keyboard.down('w');
+    for (var i = 0; i < 100; i++) {
+      joinPlayer();
+    }
 
-    // wait for the play again btns to appear (game ended)
-    await instance0.page.waitForSelector('#playAgainBtn', { timeout: 20000 });
-    await instance1.page.waitForSelector('#playAgainBtn', { timeout: 20000 });
-
-    // long timeout because it takes a while for players to die
-  }).timeout(20000);
-
-  it('players can click play again button', async function () {
-    await instance0.page.click('#playAgainBtn', { timeout: 200 });
-    await instance1.page.click('#playAgainBtn', { timeout: 200 });
-  });
-
-  it('players can join quick match again', async function () {
-    await joinQuickMatch(instance0);
-    await joinQuickMatch(instance1);
-  });
-
-
-  it('players can die to planet', async function () {
-    // register that the players have hit thrust backward
-    await instance0.page.keyboard.down('s');
-    await instance1.page.keyboard.down('s');
-
-    // register that the players have released thrust backward 5 secs later
-    setTimeout(async () => {
-      await instance0.page.keyboard.up('s');
-      await instance1.page.keyboard.up('s');
-    }, 4000);
-
-    // wait for the play again btns to appear (game ended)
-    await instance0.page.waitForSelector('#playAgainBtn', { timeout: 20000 });
-    await instance1.page.waitForSelector('#playAgainBtn', { timeout: 20000 });
-
-
-    // long timeout because it takes a while for players to die
-  }).timeout(20000);
-
-
-  it('windows can be closed', async function () {
-    // tests are over close the windows
-    await instance0.browser.close();
-    await instance1.browser.close();
-  });
+  }).timeout(20000000);
 });
