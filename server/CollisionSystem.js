e2767efce199ba5fa4212ff9b4683a83befd924a
Devon Murphy <devonmurphy1@gmail.com>
2020-02-23 18:04:43 -0800

    use Collions.js instead of own collider

--- server/CollisionSystem.js
+++ server/CollisionSystem.js
@@ -1,4 +1,4 @@
-var CollisionGrid = require('./CollisionGrid.js');
+var Collisions = require('./Collisions.js');
 
 // Class used to check the hit boxes
 class CollisionSystem {
@@ -8,30 +8,58 @@ class CollisionSystem {
         this.mapRadius = mapRadius;
         this.map = [];
         this.objects = [];
-        this.createMap();
-    }
-
-    createMap() {
-        var overlap = 1;
-        // Create Map grid square that has sides of gridCount
-        for (var x = -this.gridCount; x <= this.gridCount; x++) {
-            for (var y = -this.gridCount; y <= this.gridCount; y++) {
-                var grid = new CollisionGrid(overlap * x * this.gridSize - this.gridSize / 2, overlap * y * this.gridSize - this.gridSize / 2, this.gridSize);
-                this.map.push(grid);
-            }
-        }
     }
 
     // Iterate through all of the grids to see if there are collisions
     updateCollisions() {
         // Reset the collisions to get new ones
         this.collisions = [];
-        for (var i = 0; i < this.map.length; i++) {
-            var grid = this.map[i];
-            grid.collisions = [];
-            grid.getObjectsInRange(this.objects);
-            grid.getCollisions();
-            this.collisions = this.collisions.concat(grid.collisions);
+        // Create the collision system
+        const system = new Collisions.Collisions();
+        // Create a Result object for collecting information about the collisions
+        const result = system.createResult();
+
+        // Add all the objects to the collision system as circles
+        let colliders = [];
+        for (var i = 0; i < this.objects.length; i++) {
+            let o = this.objects[i];
+            let c = system.createCircle(o.x, o.y, o.radius);
+            c.id = o.id;
+            c.uid = o.uid;
+            c.type = o.type;
+            c.health = o.health;
+            colliders.push(c);
+        }
+
+        // Update the collision system and get potential collisions
+        system.update();
+
+        // Loop though each collider
+        for (var i = 0; i < colliders.length; i++) {
+            let c = colliders[i];
+            let o = this.objects[i];
+            const potentials = c.potentials();
+            // Loop through the potential collisions
+            for (const collision of potentials) {
+                if (c.collides(collision, result)) {
+                    // Only count collisions that are not between a player's own bullets and themselves
+                    if (!(c.id === collision.id && c.type !== collision.type)) {
+                        this.collisions = this.collisions.concat({
+                            id: c.id,
+                            uid: c.uid,
+                            type: c.type,
+                            x: c.x,
+                            y: c.y,
+                            radius: c.radius,
+                            health: c.health,
+                            hitBy: {
+                                id: collision.id,
+                                type: collision.type,
+                            }
+                        });
+                    }
+                }
+            }
         }
     }
 
